bde69e4c4b4aa0c808b25321f1586e71a23dccc6
txh18 <txh18@apple.(none)>
2013-11-20 11:45:07 +0800

    ddecode

--- tools/statNN.sh
+++ tools/statNN.sh
@@ -3,25 +3,13 @@ if [[ $# -ne 1 ]]; then
     exit;
 fi
 NN=$1
-copy_NN_local=1
 
-if [[ $copy_NN_local == 1 ]]; then
-    echo "copy || $1 || to local nnet.final..."
-    cp $NN ./nnet.final
-    NN=nnet.final
-    echo copy complete
-fi
-
-HiddenLB=$(( $(grep 'm ' $1 | wc -l) - 1 ))
+HiddenLB=$(( $(grep 'm ' $NN | wc -l) - 1 ))
 echo "Number of hidden layers : " $HiddenLB
-SS=$(grep 'm ' $1 | awk ' BEGIN { s=""; } { if (NR==1) s=s""$3; else s=s"_"$2; } END { print s } ')
+SS=$(grep 'm ' $NN | awk ' BEGIN { s=""; } { if (NR==1) s=s""$3; else s=s"_"$2; } END { print s } ')
 echo "NN topology : " $SS
-Mnum=$(grep 'm ' $1 | awk ' BEGIN { sum = 0; } { sum = sum + $2 * $3; } END { print sum; } ')
+Mnum=$(grep 'm ' $NN | awk ' BEGIN { sum = 0; } { sum = sum + $2 * $3; } END { print sum; } ')
 echo "Number of parameter in transformation Matrice : " $Mnum
 
 echo "final clean up..."
-if [[ $copy_NN_local == 1 ]]; then
-    rm $NN
-fi
-
 
